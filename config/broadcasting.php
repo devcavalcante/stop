c122e5f65afa18f3611eed6d6d8a41d22f42ad8e
devcavalcante <deborabc25@gmail.com>
2023-12-05 23:14:51 -0300

    ajuste

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,12 +29,6 @@ return [
     */
 
     'connections' => [
-
-        'options' => [
-            'cluster' => 'mt1',
-            'useTLS' => true
-        ],
-
         'ably' => [
             'driver' => 'ably',
             'key' => env('ABLY_KEY'),
@@ -59,8 +53,8 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'cluster' => 'mt1',
+                'useTLS' => true
             ],
         ],
 
