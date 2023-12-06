7dae2bcc3a85e1a075a51be9cac49bab3e0629ef
devcavalcante <deborabc25@gmail.com>
2023-12-05 23:05:33 -0300

    ajuste

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -28,7 +28,7 @@ class RoomController extends Controller
         $room = Room::where(['pin' => (int) $pin])->first();
         if(!is_null($room)) {
             $created = User::create(['pin' => $pin, 'name' => $user]);
-            broadcast(new JoinRoomMessage($user, $pin))->toOthers();
+            event(new JoinRoomMessage($user, $pin));
             return response()->json($created);
         }
         return response()->json(['message' => 'Sala não encontrada']);
