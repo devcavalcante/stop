77bc31f4d0eeec1a345c8d4d94f72707039c328b
devcavalcante <deborabc25@gmail.com>
2023-12-05 23:13:57 -0300

    ajuste

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -53,6 +53,17 @@ return [
             'driver' => 'null',
         ],
 
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => true,
+            ],
+        ],
+
     ],
 
 ];
